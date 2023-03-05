cb087c2ef7c9ca74d50c578ad0c16925216cdd69
PeaFunk3000 <80793315+PeaFunk3000@users.noreply.github.com>
2023-03-05 18:22:04 +0000

    app created + basic router navbar

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Experience from './pages/Experience';
+import Contact from './pages/Contact';
+import Navbar from './components/Navbar';
+
+function App() {
+  return ( 
+    <div className="App">
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/projects" element={<Projects />}/>
+          <Route path="/experience" element={<Experience />}/>
+          <Route path="/contact" element={<Contact/>}/>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
