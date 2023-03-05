671a7e4b7c8061bdafa9c61882dbefaaf6512d7c
PeaFunk3000 <80793315+PeaFunk3000@users.noreply.github.com>
2023-03-05 19:17:55 +0000

    expandnavbar logic and FAIcon install

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,29 @@
-import React from 'react'
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 function Navbar() {
+    const [expandNavbar, setExpandNavbar] = useState(false);
+ 
   return (
-    <div className='navbar'>  
+    <div className="navbar" id={expandNavbar ? "open" : "close"}>  
     <div className="toggleButton">
-        <button></button>
+        <button onClick={() => {
+            setExpandNavbar((prev) => !prev)
+        }}>
+            <FontAwesomeIcon icon={faBars}/>
+        </button>
     </div>
     <div className="links">
         <Link to="/">Home</Link>
         <Link to="/projects">Projects</Link>
         <Link to="/experience">Experience</Link>
         <Link to="/contact">Contact</Link>
-    </div>    
-
     </div>
+    </div>    
   )
 }
 
-export default Navbar
+export default Navbar;
