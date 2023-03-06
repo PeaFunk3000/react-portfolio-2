ac47bc2464d35eaecc5632718d221582f9127cb5
PeaFunk3000 <80793315+PeaFunk3000@users.noreply.github.com>
2023-03-06 09:26:11 +0000

    footer styled

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faInstagram, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-
+import "../styles/Footer.css"
 
 
 
@@ -10,10 +10,10 @@ import { faInstagram, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-
 export default function Footer() {
   return (
     <div className="footer">
-        <div>
-        <FontAwesomeIcon icon={faInstagram} size="6x"/>
-        <FontAwesomeIcon icon={faGithub} size="6x"/>
-        <FontAwesomeIcon icon={faLinkedin} size="6x"/>
+        <div className="socialMedia">
+        <FontAwesomeIcon icon={faInstagram} size="10x"/>
+        <FontAwesomeIcon icon={faGithub} size="10x"/>
+        <FontAwesomeIcon icon={faLinkedin} size="10x"/>
         </div>
         <p>&copy; 2023 Tom Brine</p>
     </div>
