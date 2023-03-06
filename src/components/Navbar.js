9e0504735db023a9a5b5fa1ab9ac2c5d4e297469
PeaFunk3000 <80793315+PeaFunk3000@users.noreply.github.com>
2023-03-06 09:18:43 +0000

    navbar hooks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,26 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import "../styles/Navbar.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 function Navbar() {
+    // create state to determine if navbar expanded or not via boolean
     const [expandNavbar, setExpandNavbar] = useState(false);
+
+    // get information on current route, defining location variable set from useLocation hook
+    const location = useLocation();
+
+    // whenever location changes, set expandNavbar to false to close navbar
+    useEffect(() => {
+        setExpandNavbar(false);
+    }, [location])
  
   return (
+    // navbar id to control open/close via CSS using   state hook
     <div className="navbar" id={expandNavbar ? "open" : "close"}>  
     <div className="toggleButton">
+        {/* onclick for button to expand or collapse navbar */}
         <button onClick={() => {
             setExpandNavbar((prev) => !prev)
         }}>
