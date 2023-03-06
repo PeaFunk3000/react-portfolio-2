637bf3d0dfb68a08e86cfbc35fcbd80d3a66707b
PeaFunk3000 <80793315+PeaFunk3000@users.noreply.github.com>
2023-03-06 10:47:05 +0000

    home page icons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,34 +1,43 @@
 import React from 'react';
+import prof from "../imgs/prof.png"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faHtml5, faCss3, faJs, faReact, faBootstrap, faNodeJs } from '@fortawesome/free-brands-svg-icons';
 import "../styles/Home.css"
+
 
 
 function Home() {
   return (
     <div className="home">
       <div className="about">
-        <h2>Greetings, My Name is Tom</h2>
+        <h2>Hi, My Name is Tom</h2>
         <div className="prompt"> 
-        <p>A self-proclaimed designer-maker!</p>
-        <FontAwesomeIcon icon={faEnvelope} />
-        <FontAwesomeIcon icon={faGithub} />
-        <FontAwesomeIcon icon={faLinkedin} />
+        <p>A Junior Front-End Developer!</p>
+        <img id="headshot" src={prof}></img>
         </div>
       </div>
     <div className="skills">
       <h1>Skills</h1>
-      <ol className="list">
-        <li className="item">
+      <div className="list">
+        <div className="item">
           <h2>Front-End</h2>
+          <div className="skillIcons">
+          <FontAwesomeIcon icon={faReact} />
+          <FontAwesomeIcon icon={faBootstrap} />
+          <FontAwesomeIcon icon={faNodeJs} />
+          </div>
           <span>ReactJS, Bootstrap, nodeJS, jQuery</span>
-          </li>
-          <li className="item">
+          </div>
+          <div className="item">
           <h2>Languages</h2>
+          <div className="skillIcons">
+          <FontAwesomeIcon icon={faHtml5} />
+          <FontAwesomeIcon icon={faCss3} />
+          <FontAwesomeIcon icon={faJs} />
+          </div>
           <span>HTML, CSS, JavaScript</span>
-        </li>
-      </ol>
+        </div>
+      </div>
     </div>
     </div>
   )
