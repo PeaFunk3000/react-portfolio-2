cbbbfc2771f202238f24a700ffb0dca076c2d178
PeaFunk3000 <80793315+PeaFunk3000@users.noreply.github.com>
2023-03-06 13:50:01 +0000

    href links for socials

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,15 +2,16 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faInstagram, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import "../styles/Footer.css"
+import socials from "../socials.json";
 
 
 export default function Footer() {
   return (
     <div className="footer">
         <div className="socialMedia">
-        <FontAwesomeIcon icon={faInstagram} />
-        <FontAwesomeIcon icon={faGithub} />
-        <FontAwesomeIcon icon={faLinkedin} />
+        <a href={socials.instagram} target="_blank"><FontAwesomeIcon icon={faInstagram} /></a>
+        <a href={socials.github} target="_blank"><FontAwesomeIcon icon={faGithub} /></a>
+        <a href={socials.linkedin} target="_blank"><FontAwesomeIcon icon={faLinkedin} /></a>
         </div>
         <p>&copy; 2023 Tom Brine</p>
     </div>
